a6745d6b5948d4b3124110ff589a505a65418d5d
Althaf <alphadace@gmail.com>
2018-07-28 14:15:10 +0530

    chat done

---
+++ Anonymi/app/src/main/java/com/anonymi/anonymi/Fragments/Chat/ChatMessage.java
@@ -0,0 +1,11 @@
+package com.anonymi.anonymi.Fragments.Chat;
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left, String message) {
+        super();
+        this.left = left;
+        this.message = message;
+    }
+}
